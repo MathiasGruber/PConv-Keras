aee9af20594879d95ffdac13b858419f26df68fe
Mathias Felix Gruber <mafg@novozymes.com>
2018-06-11 16:30:29 +0200

    Add channels to mask

--- libs/util.py
+++ libs/util.py
@@ -4,9 +4,9 @@ import numpy as np
 import cv2
 
 
-def random_mask(height, width):
+def random_mask(height, width, channels=3):
     """Generates a random irregular mask with lines, circles and elipses"""    
-    img = np.zeros((height, width), np.uint8)
+    img = np.zeros((height, width, channels), np.uint8)
 
     # Set size scale
     size = int((width + height) * 0.03)
@@ -18,13 +18,13 @@ def random_mask(height, width):
         x1, x2 = randint(1, width), randint(1, width)
         y1, y2 = randint(1, height), randint(1, height)
         thickness = randint(3, size)
-        cv2.line(img,(x1,y1),(x2,y2),1,thickness)
+        cv2.line(img,(x1,y1),(x2,y2),(1,1,1),thickness)
         
     # Draw random circles
     for _ in range(randint(1, 20)):
         x1, y1 = randint(1, width), randint(1, height)
         radius = randint(3, size)
-        cv2.circle(img,(x1,y1),radius,1, -1)
+        cv2.circle(img,(x1,y1),radius,(1,1,1), -1)
         
     # Draw random ellipses
     for _ in range(randint(1, 20)):
@@ -32,6 +32,6 @@ def random_mask(height, width):
         s1, s2 = randint(1, width), randint(1, height)
         a1, a2, a3 = randint(3, 180), randint(3, 180), randint(3, 180)
         thickness = randint(3, size)
-        cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,1, thickness)
+        cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,(1,1,1), thickness)
     
     return 1-img
