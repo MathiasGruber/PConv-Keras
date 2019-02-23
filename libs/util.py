97b7f85deb2d6eacf6760ef0d4089a751ba59271
Mathias Gruber <nano.mathias@gmail.com>
2019-02-23 15:04:16 +0100

    Load masks from disk

--- libs/util.py
+++ libs/util.py
@@ -1,12 +1,16 @@
-from random import randint
+from random import randint, seed
 import itertools
 import numpy as np
 import cv2
 
 
-def random_mask(height, width, channels=3):
+def random_mask(height, width, channels=3, rand_seed=None):
     """Generates a random irregular mask with lines, circles and elipses"""    
+    
     img = np.zeros((height, width, channels), np.uint8)
+
+    # Seed for reproducibility
+    seed(rand_seed)
 
     # Set size scale
     size = int((width + height) * 0.03)
@@ -35,3 +39,30 @@ def random_mask(height, width, channels=3):
         cv2.ellipse(img, (x1,y1), (s1,s2), a1, a2, a3,(1,1,1), thickness)
     
     return 1-img
+
+
+def load_mask(filepath, height, width, rotation=None, dilation=None, cropping=None):
+    """Loads a mask from disk, and optionally augments it"""
+    
+    # Read image
+    mask = cv2.imread(filepath)
+    
+    # Random rotation
+    if rotation:
+        rand = np.random.randint(-180, 180)
+        M = cv2.getRotationMatrix2D((mask.shape[1]/2, mask.shape[0]/2), rand, 1.5)
+        mask = cv2.warpAffine(mask, M, (mask.shape[1], mask.shape[0]))
+        
+    # Random dilation
+    if dilation:
+        rand = np.random.randint(5, 47)
+        kernel = np.ones((rand, rand), np.uint8) 
+        mask = cv2.erode(mask, kernel, iterations=1)
+        
+    # Random cropping
+    if cropping:
+        x = np.random.randint(0, mask.shape[1] - width)
+        y = np.random.randint(0, mask.shape[0] - height)
+        mask = mask[y:y+height, x:x+width]
+
+    return (mask > 1).astype(np.uint8)
